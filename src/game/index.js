6a9d8f380c5526695d1574b8189718954dcf6300
w-t-w <15866369958@163.com>
2024-11-11 16:12:11 +0800

    feature: 使用 nodejs 实现一个简单的石头、剪刀与布的游戏

---
+++ src/game/index.js
@@ -0,0 +1,31 @@
+const argv = process.argv,
+    length = argv.length;
+
+const gameSelection = ["scissor", "rock", "paper"],
+    selectionLength = gameSelection.length;
+
+const playerAction = argv[length - 1];
+
+if (!gameSelection.includes(playerAction)) {
+    console.log("玩家输入的剪刀、石头与布猜拳手势为空!请重新输入!");
+    process.exit(0);
+}
+
+let position = 0;
+
+const computerRandom = Math.floor(Math.random() * selectionLength);
+
+position = computerRandom + 1;
+if (position === selectionLength) position = 0;
+
+const computerAction = gameSelection[computerRandom];
+
+console.log(`玩家: ${playerAction}, 电脑: ${computerAction}`);
+
+if (computerAction === playerAction) {
+    console.log("平局!");
+} else if (gameSelection[position] === playerAction) {
+    console.log("玩家赢!电脑输!");
+} else {
+    console.log("电脑赢!玩家输!");
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+console.log("第一章(nodejs 内置对象与方法):");
+console.log("");
 require('./basic/index');
+console.log("第二章(nodejs 手写一个简单的剪刀、石头与布猜拳游戏):");
+console.log("");
+require('./game/index');
