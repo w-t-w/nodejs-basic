7ef9526203d1980fe5475eda85cd245a4ab319b1
w-t-w <15866369958@163.com>
2024-11-11 15:58:19 +0800

    feature: nodejs 中的内置对象与方法

--- src/basic/index.js
+++ src/basic/index.js
@@ -1,1 +1,36 @@
+console.log("hello world");
 
+console.log(Date);
+console.log(Math);
+
+console.log(setTimeout);
+console.log(setInterval);
+// console.log(requestAnimationFrame);
+console.log(setImmediate);
+
+console.log(__filename);
+console.log(__dirname);
+console.log(process.cwd());
+
+console.log(process);
+console.log(process.version);
+console.log(process.versions);
+console.log(process.title);
+console.log(process.description);
+console.log(process.env);
+console.log(process.arch);
+console.log(process.platform);
+console.log(process.argv);
+console.log(process.argv0);
+console.log(process.pid);
+console.log(process.ppid);
+console.log(process.hrtime);
+console.log(process.nextTick);
+console.log(process.kill);
+console.log(process.exit);
+console.log(process.cwd);
+console.log(process.chdir);
+console.log(process.abort);
+console.log(process.cpuUsage);
+console.log(process.memoryUsage);
+console.log(process.resourceUsage);
