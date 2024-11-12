cfee70d27a3387b4ad91a8710e77ec8a84f81553
w-t-w <15866369958@163.com>
2024-11-12 10:10:03 +0800

    feature: rpc 简单的全双工通信

--- src/rpc/doublePlexSimple/server.js
+++ src/rpc/doublePlexSimple/server.js
@@ -54,3 +54,5 @@
 // server.listen(PORT, function () {
 //     console.log(`The server is listening at http://localhost:${PORT}!`);
 // });
+
+console.log("rpc 简单的全双工通信课程已经结束!如需重新学习,则将本文件上面的所有被注释的代码解注即可!");
